fa21dd6e3d3f99bd8a5ff90558a1f2677d8e840e
Frank Blaauw <frank.blaauw@gmail.com>
2017-11-09 13:11:36 +0100

    Implemented babel for transpiling

--- webpack.common.js
+++ webpack.common.js
@@ -38,6 +38,15 @@ module.exports = {
         ],
       },
     ],
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
   },
   resolve: {
     extensions: ['.js', '.jsx'],
